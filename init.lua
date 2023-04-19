e2083d8e7e99cf03fce6b18a83c8d8ffe1b8f972
Rustam Gasanov <ragasanov@gmail.com>
2023-04-19 12:12:21 +0200

    Add go and ruby LSP

--- init.lua
+++ init.lua
@@ -58,3 +58,21 @@ nmap("<Leader>f", "<CMD>Telescope find_files<CR>")
 require("toggleterm").setup{ direction = "horizontal", size = 15, open_mapping = [[<C-\>]] }
 
 vim.cmd("colorscheme tokyonight-storm")
+
+-- LSP
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.ensure_installed({
+  -- Replace these with whatever servers you want to install
+  'gopls',
+  'solargraph',
+})
+
+-- (Optional) Configure lua language server for neovim
+-- require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
