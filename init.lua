d7153f8598ec7f1b1fc1da92f05c6248caf4c893
Rustam Gasanov <ragasanov@gmail.com>
2023-05-26 16:45:52 +0200

    Add imports *after* fmt

--- init.lua
+++ init.lua
@@ -31,8 +31,8 @@ vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
   pattern = { '*.go' },
   callback = function()
-    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
     vim.lsp.buf.format()
+    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
   end
 })
 
